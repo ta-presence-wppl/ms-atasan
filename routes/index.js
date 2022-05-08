a534e343a508308b00690087f687d8003db74480
AhmadR <ahmad.cos27@gmail.com>
2022-05-08 11:21:11 +0700

    addRoute

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,22 @@ const router = express.Router();
 //Define Validasi Auth
 const authChecker = require('../services/auth_check');
 
-//Define Root Absent Out
+//Define Root Absent History
 const routerHistory = require('./absents/history');
 
-//Define API /absent/status
+//Define Root Izin History
+const routerHistoryIzin = require('./izin/history');
+
+//Define API /absent/history
 router.use('/absent/history', authChecker.checkAuth, (req, res, next) => {
     next();
 }, routerHistory);
+
+//Define API /izin/history
+router.use('/izin/history', authChecker.checkAuth, (req, res, next) => {
+    next();
+}, routerHistoryIzin);
+
 
 //Sample Route
 /**
